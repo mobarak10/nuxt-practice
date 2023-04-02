ad1e34484f09906fc7bd01b0ae86aca8eb63b8d5
Mobarak Hossain <mobarak.hossain.cse12@gmail.com>
2023-04-02 20:01:32 +0600

    initial commit with class 3

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus modi corporis eaque, numquam amet nam tenetur nostrum accusantium incidunt dolorum nihil perferendis autem, vitae illo aliquam unde! Laboriosam, voluptatem blanditiis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime vero modi repudiandae officiis fugit id voluptates? Numquam, praesentium! Neque dolorum quia odit a! Aliquid deleniti ducimus dolor at nobis beatae?</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
