82dfd4c51925c0fbd934b63507e4c7a9b040a87f
Mobarak Hossain <mobarak.hossain.cse12@gmail.com>
2023-04-03 21:20:03 +0600

    class-4 done

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo animi, sit, fugit consectetur facilis cupiditate ratione facere illo ipsum et error ipsam laborum officiis cumque architecto quidem dolore doloremque totam?</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
